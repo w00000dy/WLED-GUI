cd246db18d569e63c6120955fa7dc4869f3d8d13
Woody <27882680+WoodyLetsCode@users.noreply.github.com>
2021-01-15 12:30:30 +0100

    improved updater & main process

--- js/updater.js
+++ js/updater.js
@@ -1,14 +1,16 @@
 // set version
 const wledGuiVersion = require('electron').remote.app.getVersion();
 
-checkForUpdate();
+if (localStorage.getItem("remindLaterTime") === null || (Date.now() - localStorage.getItem("remindLaterTime")) >= 259200000) {  // 3 days
+    checkForUpdate();
+}
 
 // checks if a update is available
 function checkForUpdate() {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://raw.githubusercontent.com/WoodyLetsCode/WLED-GUI/master/VERSION', true);
     xhr.onload = function () {
-        if (xhr.response !== wledGuiVersion && (localStorage.getItem("remindLaterTime") === null || (Date.now() - localStorage.getItem("remindLaterTime")) >= 259200000)) { // 3 days
+        if (xhr.response !== wledGuiVersion) {
             log.info("New update avaiable!");
             let instance = M.Modal.getInstance(document.getElementById("updatePopup"));
             document.getElementById("updatePopupText").innerText = "A new update for WLED-GUI is available.\n\nYour version: " + wledGuiVersion + "\nLatest version: " + xhr.response;
