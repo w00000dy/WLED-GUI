5c0a926a6de1a65ad63de46114bce3a7034a13db
Woody <27882680+WoodyLetsCode@users.noreply.github.com>
2021-01-16 19:12:10 +0100

    disable start of second instance

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ log.info('WLED-GUI started');
 log.debug("Start arguments:");
 log.debug(process.argv);
 
+const gotTheLock = app.requestSingleInstanceLock()
 const autostarted = process.argv.indexOf('--hidden') !== -1;
 const dev = process.argv.indexOf('--dev') !== -1;
 
@@ -165,29 +166,42 @@ function checkTray() {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-app.whenReady().then(loadSettings)
+// check if second instance was started
+if (!gotTheLock) {
+  log.info('WLED-GUI quitted');
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (win) {
+      log.info('Someone tried to run a second instance. Focus our main window');
+      win.show()
+    }
+  })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    log.info('WLED-GUI quitted');
-    app.quit()
-  }
-})
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(createWindow)
+  app.whenReady().then(loadSettings)
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      log.info('WLED-GUI quitted');
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
